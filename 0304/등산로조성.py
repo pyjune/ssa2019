1867dcf389ffdb3eb0a2bff97840d0175bb90174
pyjune <pyjune@naver.com>
2019-03-04 13:30:22 +0900

    Update 등산로조성.py

--- 0304/등산로조성.py
+++ 0304/등산로조성.py
@@ -4,17 +4,20 @@ def f(i, j, c, k, n): # i, j 좌표,  n: 크기
     global maxV
     if(maxV < c):
         maxV = c
+    visited[i][j] = 1
     for p in range(4): #탐색할 방향
         ni = i + di[p]
         nj = j + dj[p]
         if(ni>=0 and ni<n and nj>=0 and nj<n): #영역을 벗어나지 않고
-            if(h[i][j] > h[ni][nj]): #더 낮은 곳으로 이동
-                f(ni, nj, c+1, k, n)
-            elif(h[i][j] > h[ni][nj]-k):  # 깎아서 이동
-                pre = h[ni][nj]
-                h[ni][nj] = h[i][j] - 1 # k만큼 깎는게 아니라 이동에 꼭필요한 차이만큼만
-                f(ni, nj, c+1, 0, n)
-                h[ni][nj] = pre
+            if (visited[ni][nj] != 1): # 지나온 경로를 깎는 것을 방지
+                if(h[i][j] > h[ni][nj]): #더 낮은 곳으로 이동
+                    f(ni, nj, c+1, k, n)
+                elif(h[i][j] > h[ni][nj]-k):  # 깎아서 이동
+                    pre = h[ni][nj]
+                    h[ni][nj] = h[i][j] - 1
+                    f(ni, nj, c+1, 0, n)
+                    h[ni][nj] = pre
+    visited[i][j] = 0
 
 T = int(input())
 for tc in range(1, T+1):
@@ -23,6 +26,7 @@ for tc in range(1, T+1):
     maxV = 0
     si = 0
     sj = 0
+    visited = [[0]*N for i in range(N)]
     for i in range(N):
         for j in range(N):
             if(h[si][sj] < h[i][j]):
